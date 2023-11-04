081f60d15f0926cb65fae2accb2b909a223f4812
Milko Milchev <milkomilchev@Milkos-MacBook-Pro.local>
2023-11-04 09:52:56 +0200

    AWS deployment

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React to test 
+            Learn React test user
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
